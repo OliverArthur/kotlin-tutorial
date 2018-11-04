1e1d32e2dc472756f8d882d8e71dd0999c98812e
Oliver Arthur <oliverarthurnardi@gmail.com>
2018-11-04 22:47:11 +0000

    first commit

---
+++ string.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    
+    var firstname:String = "Oliver"
+    var lastname:String = "Arthur"
+
+    println("Hi my name is $firstname $lastname and my full name contain ${firstname.length + lastname.length} characters")
+}
